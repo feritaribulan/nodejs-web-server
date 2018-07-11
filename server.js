930b4f13f873ad6c704798c9710dbb1ee133069b
Ferit ARIBULAN <faribulan@stu.okan.edu.tr>
2018-07-11 21:03:57 +0300

    MiddleWare ayrıldı.

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
 var express = require ("express");
+var middleware = require("./middleware");
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-    requireAuthentication: function(req, res, next){
-        console.log("Özel root girildi.");
-        next();
-    }, logger: function(req, res, next){
-        console.log(req.method + " " + req.originalUrl);
-        next();
-    }
-}
 
 app.use(middleware.logger);
-
 app.get("/hakkimda", middleware.requireAuthentication, function(req, res){
     res.send("hakkımda");
 })
-
 app.use(express.static(__dirname + '/public'))
-
 app.listen(PORT, function(){
     console.log("Web Servis Başlatıldı. Port : " + PORT);
 });
